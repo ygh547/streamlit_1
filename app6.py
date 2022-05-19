7080dbb31c878793f9e177534278def9e8c9e015
ygh547 <ygh547@naver.com>
2022-05-19 17:19:53 +0900

    add

--- app6.py
+++ app6.py
@@ -1,3 +1,4 @@
+from email import message
 import streamlit as st
 
 def main() :
@@ -7,6 +8,41 @@ def main() :
     name = st.text_input('이름을 입력하세요')
     if name != '':
         st.subheader(name + '님 안녕하세요')
+
+    # 2. 입력 글자 갯수 제한
+    address = st.text_input('주소를 입력하세요', max_chars=10)
+    st.subheader(address)
+
+    # 3. 여러 행을 입력가능하도록
+    message = st.text_area('메세지를 입력하세요', height=3)
+    st.subheader(message)
+
+    # 4. 숫자 입력 , 정수
+    st.number_input('숫자를 입력하세요', 1,100)
+
+    # 5. 숫자 입력 , 실수
+    st.number_input('숫자를 입력하세요',1.0,100.0,30.0,0.8)
+
+    # 6. 날짜 입력
+    my_date = st.date_input('약속날짜')
+    st.write(my_date)
+
+    # 요일 알아보기
+    st.write( my_date.weekday())
+    st.write( my_date.strftime('%A'))
+    
+    # 7. 시간 입력
+    my_time = st.time_input('시간을 입력하세요')
+    st.write(my_time)
+    
+    # 8. 색깔 입력
+    my_color = st.color_picker('색을 선택하세요')
+    st.write(my_color)
+
+    # 9. 비밀번호 입력
+    my_password = st.text_input('비밀번호 입력하세요', type= 'password')
+    st.write(my_password)
+
     
 
 
