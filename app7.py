2317474aff9e4e6f3bfb6e1311d11a7737a7bf3f
ygh547 <ygh547@naver.com>
2022-05-20 09:44:57 +0900

    add

--- app7.py
+++ app7.py
@@ -34,19 +34,31 @@ def main():
             print(upload_file.size)
             print(upload_file.type)
 
-        # 파일명을 유니크하게 만들어서 저장해야 한다.
-        # 현재시간을 활요해서 파일명을 만든다.
-        current_time = datetime.now()
-        print(current_time.isoformat().replace(':','_'))
-        
-        new_filename = current_time.isoformat().replace(':','_') + '.jpg'
+            # 파일명을 유니크하게 만들어서 저장해야 한다.
+            # 현재시간을 활요해서 파일명을 만든다.
+            current_time = datetime.now()
+            print(current_time.isoformat().replace(':','_'))
+            
+            new_filename = current_time.isoformat().replace(':','_') + '.jpg'
 
-        upload_file.name = new_filename
-        save_uploaded_file('temp', upload_file)
+            upload_file.name = new_filename
+            save_uploaded_file('temp', upload_file)
 
 
     elif choice == menu[1] :
         st.subheader('CSV 파일 업로드')
+        upload_file = st.file_uploader('CSV 파일 선택', type=['CSV'])
+        if upload_file is not None :
+            
+            # 파일명을 유니크하게 만들어서 저장해야 한다.
+            # 현재시간을 활요해서 파일명을 만든다.
+            current_time = datetime.now()
+            print(current_time.isoformat().replace(':','_'))
+            
+            new_filename = current_time.isoformat().replace(':','_') + '.CSV'
+
+            upload_file.name = new_filename
+            save_uploaded_file('temp', upload_file)
 
     else :
         st.subheader('파일 업로드 프로젝트 입니다.')
